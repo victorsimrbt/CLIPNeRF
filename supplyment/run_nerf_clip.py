103e3ee6407e82164eb3a08293c44c032958eaaa
Victor Sim <victorwtsim@gmail.com>
2022-10-28 13:35:46 +0100

    Video Saving

--- supplyment/run_nerf_clip.py
+++ supplyment/run_nerf_clip.py
@@ -564,7 +564,7 @@ def config_parser():
                         help='frequency of weight ckpt saving')
     parser.add_argument("--i_testset", type=int, default=50000,
                         help='frequency of testset saving')
-    parser.add_argument("--i_video",   type=int, default=50000,
+    parser.add_argument("--i_video",   type=int, default=10000,
                         help='frequency of render_poses video saving')
     parser.add_argument("--N_iters", type=int, default=200000, 
                         help='number of iters')
